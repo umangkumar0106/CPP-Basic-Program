b7af1c736ce5a01026d303077f6ceab10cd2763f
Umang Kumar <136004708+umangkumar0106@users.noreply.github.com>
2025-11-11 01:57:38 +0530

    Add files via upload

---
+++ Pyramid_taiangle.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of rows";
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=0;k<=i;k++)
+        {
+            cout<<"*";
+        }
+        for(int g=0;g<i;g++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+    
+}
