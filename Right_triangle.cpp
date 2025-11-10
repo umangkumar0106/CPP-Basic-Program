0f8550876c0855c3207abcdc4c74a8c84ca6e88d
Umang Kumar <136004708+umangkumar0106@users.noreply.github.com>
2025-11-10 23:14:03 +0530

    Add files via upload

---
+++ Right_triangle.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of rows";
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=1;j<n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=0;k<=i;k++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
