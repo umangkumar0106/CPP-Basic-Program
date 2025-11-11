b2018dc01c532d00cdb05f0cf651043a6f532623
Umang Kumar <136004708+umangkumar0106@users.noreply.github.com>
2025-11-12 00:24:04 +0530

    Add files via upload

---
+++ Daimondpattern.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of rows";
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=0;k<=i;k++)
+        {
+            cout<<"*";
+        }
+        for(int h=1;h<=i;h++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+     for(int i=n;i>=0;i--)
+    {
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=0;k<=i;k++)
+        {
+            cout<<"*";
+        }
+        for(int h=1;h<=i;h++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
